c9fd4ea14e0e6660788b7227d08fc632a4fa1232
Viktory <vika.scheglowa@yandex.ru>
2024-08-06 14:34:28 +0300

    fixed errors

--- src/widget.py
+++ src/widget.py
@@ -3,10 +3,10 @@ from src.masks import get_mask_card_number, get_mask_account
 
 def mask_account_card(nums: str) -> str:
     if "Счет" in nums:
-        return f"{nums[:-20]}{'*' * 2}{nums[-4::]}"
+        return f"{nums[:-20]} **{nums[-4::]}"
 
     else:
-        return f"{nums[:-12]} {nums[-12:-14]}{'*' * 2} {'*' * 4} {nums[-4::]}"
+        return f"{nums[:-12]} {nums[-12:-14]}** **** {nums[-4::]}"
 
 
 def get_date(date: str) -> str:
