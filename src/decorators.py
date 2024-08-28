5fc7a01e477279f94fb34d359164e542dd1c8259
Viktory <vika.scheglowa@yandex.ru>
2024-08-28 15:10:40 +0300

    homework is done2

--- src/decorators.py
+++ src/decorators.py
@@ -1,34 +1,23 @@
-from functools import wraps
-from typing import Any, Callable
+def log(filename=""):
+    """Decorator create log about function operation."""
 
-
-def log(filename: str | None = None) -> Callable:
-    """Логирует начало и конец выполнения функции и ее результат или ошибки в файл или в консоль."""
-
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
+    def my_decorator(func):
+        def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                log_message = f"{func.__name__} called with args: {args}, kwargs:{kwargs}. Result: {result}"
-                with open(filename, "a") as file:
-                    file.write(log_message + "\n")
-                print(log_message)
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} ok")
+                else:
+                    print(f"{func.__name__} ok")
+                return result
             except Exception as e:
-                error_message = f"{func.__name__} error: {e}. Inputs:{args}, {kwargs}"
-                with open(filename, "a") as file:
-                    file.write(error_message + "\n")
-                print(error_message)
-            return result
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
+                else:
+                    print(f"{func.__name__} error: {e.__class__.__name__}. Inputs: {args}, {kwargs}")
 
         return wrapper
 
-    return decorator
-
-
-@log(filename="mylog.txt")
-def my_function(x: int, y: int) -> int:
-    return x + y
-
-
-print(my_function(1, 2))
+    return my_decorator
