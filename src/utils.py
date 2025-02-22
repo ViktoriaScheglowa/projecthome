66045aa5766478b4abee5dc729868c606b70acee
Viktory <vika.scheglowa@yandex.ru>
2025-02-22 21:31:53 +0300

    Merge branch 'develop' into feature/homework_10_1
    # Conflicts:
    #	.gitignore

--- src/utils.py
+++ src/utils.py
@@ -7,13 +7,9 @@ def get_transactions_dictionary(path: str) -> Any:
     """Принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
     try:
         with open(path, "r", encoding="utf-8") as operations:
-            try:
-                transactions_data = json.load(operations)
-                return transactions_data
-            except json.JSONDecodeError:
-                transactions_data = []
-                return transactions_data
-    except FileNotFoundError:
+            transactions_data = json.load(operations)
+            return transactions_data
+    except (json.JSONDecodeError.FileNotFoundError):
         transactions_data = []
         return transactions_data
 
