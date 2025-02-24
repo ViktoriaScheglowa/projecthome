4370dead0a213d41d8a9ed59d5563e6233c20935
Viktory <vika.scheglowa@yandex.ru>
2025-02-24 17:31:21 +0300

    homework is done2

--- src/external_api.py
+++ src/external_api.py
@@ -13,24 +13,18 @@ api_key = os.getenv("API_KEY")
 
 def convert_to_rub(amount: float, currency: str) -> Any:
     """Функция принимает значение в долларах или евро, обращается к API и возвращает конвертацию в рубли"""
-    try:
-        if currency == "USD":
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount={amount}"
-            headers = {"apikey": api_key}
-            response = requests.get(url, headers=headers)
-            json_result = response.json()
-            rub_amount = json.loads(json_result)["result"]
-            return rub_amount
-        elif currency == "EUR":
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=EUR&amount={amount}"
-            headers = {"apikey": api_key}
-            response = requests.get(url, headers=headers)
-            json_result = response.json()
-            rub_amount = json.loads(json_result)["result"]
-            return rub_amount
-    except RequestException:
-        return 0
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount={amount}"
+    headers = {"apikey": api_key}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise ValueError(f"Failed to get currency rate")
+
+    else:
+        json_result = response.json()
+        rub_amount = json_result["result"]
+        return rub_amount
 
 
-# if __name__ == "__main__":
-#    print(convert_to_rub(45, "USD"))
+
+if __name__ == "__main__":
+    print(convert_to_rub(45, "USD"))
