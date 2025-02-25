4ce96152d9b9b39f3e51808d7190c31f73785b63
Viktory <vika.scheglowa@yandex.ru>
2025-02-25 21:49:13 +0300

    homework is done2

--- src/external_api.py
+++ src/external_api.py
@@ -1,29 +1,27 @@
-import json
 import os
-from typing import Any
 
 import requests
 from dotenv import load_dotenv
-from requests import RequestException
 
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
 
 
-def convert_to_rub(amount: float, currency: str) -> float:
+def convert_to_rub(transaction: dict) -> float:
     """Функция принимает значение в долларах или евро, обращается к API и возвращает конвертацию в рубли"""
-    url = f"https://api.apilayer.com/exchangerates_data/convert?from': currency, 'to': 'RUB', 'amount': amount={amount}"
     headers = {"apikey": api_key}
-    response = requests.get(url, headers=headers)
+    amount = transaction["operationAmount"].get("amount")
+    code = transaction["operationAmount"]['currency']['code']
+    to = 'RUB'
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amount}"
+    response = requests.get(url, headers=headers, params={'from': code, 'to': 'RUB', 'amount': amount})
     if response.status_code != 200:
         raise ValueError(f"Failed to get currency rate")
-
     else:
         json_result = response.json()
         rub_amount = json_result["result"]
         return rub_amount
-
 
 
 if __name__ == "__main__":
