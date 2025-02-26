85da5efefb635dc311857aebd88f7c01e17a52c8
Viktory <vika.scheglowa@yandex.ru>
2025-02-26 21:36:35 +0300

    homework is done2

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,3 @@
-import json
-import requests
 import logging
 
 
@@ -19,15 +17,23 @@ logger.addHandler(file_handler)
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
+    logging.info("Start")
     if card_number.isdigit() and len(card_number) == 16:
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+        logging.info("Return of mask card number")
     else:
+        logging.error("Return  an empty string")
         return ''
+    logging.info("Stop")
 
 
 def get_mask_account(acc_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
+    logging.info("Start")
     if acc_number.isdigit() and len(acc_number) == 20:
+        logging.info("Return of mask account number")
         return f"**{acc_number[-4::]}"
     else:
+        logging.error("Return  an empty string")
         return ''
+    logging.info("Stop")
